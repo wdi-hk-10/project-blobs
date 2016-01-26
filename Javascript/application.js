16a8e35375b753c4f5347a0545e5326014587074
Shawn Alwani <shawn.alwani@gmail.com>
2016-01-26 11:21:07 +0800

    added color generator

--- Javascript/application.js
+++ Javascript/application.js
@@ -1,17 +1,56 @@
 $(document).ready(function(){
 
-var correctColor;
+var pOneCorrectColor;
+var pTwoCorrectCOlor;
 var playerWord;
 var $playerOne=$("#pOneDisplayBox");
 var $playerTwo=$("#pTwoDisplayBox");
 
-function lightColorGenerator(x){
-  var color=parseInt(Math.random()*x+1);
+function lightColorGenerator(){
+  var color=parseInt(Math.random()*5+1);
   if (color==1){
-    correctColor="blue";
+    pOneCorrectColor="blue";
+    $playerOne.css({"color":"blue"});
+  } else if (color==2){
+    pOneCorrectColor="red";
+    $playerOne.css({"color":"red"});
+  } else if (color==3){
+    pOneCorrectColor="green";
+    $playerOne.css({"color":"green"});
+  } else if (color==4){
+    pOneCorrectColor="orange";
+    $playerOne.css({"color":"orange"});
+  } else if (color==5){
+    pOneCorrectColor="yellow";
+    $playerOne.css({"color":"yellow"});
+  } else if (color==6){
+    pOneCorrectColor="purple";
+    $playerOne.css({"color":"purple"});
   }
 };
-lightColorGenerator(10);
+
+function darkColorGenerator(){
+  var color=parseInt(Math.random()*5+1);
+  if (color==1){
+    pTwoCorrectColor="blue";
+    $playerTwo.css({"color":"blue"});
+  } else if (color==2){
+    pTwoCorrectColor="red";
+    $playerTwo.css({"color":"red"});
+  } else if (color==3){
+    pTwoCorrectColor="green";
+    $playerTwo.css({"color":"green"});
+  } else if (color==4){
+    pTwoCorrectColor="orange";
+    $playerTwo.css({"color":"orange"});
+  } else if (color==5){
+    pTwoCorrectColor="yellow";
+    $playerTwo.css({"color":"yellow"});
+  } else if (color==6){
+    pTwoCorrectColor="purple";
+    $playerTwo.css({"color":"purple"});
+  }
+};
 
 function wordGenerator($player){
   var word = parseInt(Math.random()*5+1);
@@ -30,7 +69,8 @@ function wordGenerator($player){
   }
   $player.text(playerWord).css({"font-size":"35px"});
 };
-
+lightColorGenerator();
+darkColorGenerator();
 wordGenerator($playerOne);
 wordGenerator($playerTwo);
 
